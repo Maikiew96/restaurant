c28109c35f5b1433fa9ae82eedd12530cc4a36e2
Maikiew96 <32905053+Maikiew96@users.noreply.github.com>
2024-01-26 20:09:44 +0100

    Nextjs + header component + Shadcn
    Installed nextjs, shadcdn for components and basis header component

---
+++ my-app/src/app/components/Header.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import Image from 'next/image'
+import Logo from '../assets/images/logo.png'
+export const Header = () => {
+  return (
+    <header className='flex justify-around items-center h-32 border border-green-500'>
+        <div>
+            <p>Titel komt hier</p>
+        </div>
+        <div>
+            {/*Gebruik de Image component van Next.js voor optimalisatie */}
+         <Image 
+            src={Logo}
+            alt="Restaurant logo"
+            width={80}
+            height={800}
+        />
+        </div>
+        <div>
+            {/*Dark mode toggler komt hier*/}
+        </div>
+    </header>
+  )
+}
---
+++ my-app/src/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+import { Header } from "./components/Header";
+
+export default function Home() {
+  return (
+    <Header />
+   
+  );
+}
