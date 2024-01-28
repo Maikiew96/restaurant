c7051b19c446a0a699070463187827521b8ab49d
Maikiew96 <32905053+Maikiew96@users.noreply.github.com>
2024-01-28 20:35:58 +0100

    Introduction component
    Busy with the introduction component, looking for the best way to implement Fonts in Nextjs. After this change, looking to make a prizing menu or a carrousel, both with components from shadcdn, but styled to match the tone of voice of the end product.

--- my-app/src/app/components/Header.tsx
+++ my-app/src/app/components/Header.tsx
@@ -4,19 +4,18 @@ import Logo from '../assets/images/logo.png'
 import { ModeToggle } from './ui/ThemeToggler'
 export const Header = () => {
   return (
-    <header className='flex justify-around items-center h-32'>
-        <div>
-            <p>Titel komt hier</p>
-        </div>
+    <header className='flex justify-around items-center h-fit-content'>
         <div>
             {/*Gebruik de Image component van Next.js voor optimalisatie */}
          <Image 
             src={Logo}
             alt="Restaurant logo"
-            width={80}
-            height={800}
-        />
+            width={350}
+
+            // height={800}
+            />
         </div>
+     
         <div>
             {/*Dark mode toggler komt hier*/}
             <ModeToggle />
---
+++ my-app/src/app/components/Introduction.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Introduction() {
+  return (
+    <div className='flex justify-center'>
+        <h1 className='text-2xl'>Welcome to Da Pizza Italian food & drinks!</h1>
+    </div>
+  )
+}
